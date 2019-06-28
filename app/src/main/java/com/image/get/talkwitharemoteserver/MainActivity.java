f86bed4848775e9c0a77da103753bbd6a9bf7c33
iamaliabdelhai <ali.abdalhay@mail.huji.ac.il>
2019-06-28 23:24:33 +0300

    post_pc

--- app/src/main/java/com/image/get/talkwitharemoteserver/MainActivity.java
+++ app/src/main/java/com/image/get/talkwitharemoteserver/MainActivity.java
@@ -14,9 +14,7 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.squareup.picasso.Picasso;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
